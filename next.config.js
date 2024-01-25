e2110b08ea819c33971735d6d2d0bdd0ef0710af
Leigh Dahlin <leigh.dahlin@gmail.com>
2024-01-25 15:43:07 +0100

    revert previous settings and update next config with console logs

--- next.config.js
+++ next.config.js
@@ -1,14 +1,37 @@
 /** @type {import('next').NextConfig} */
 
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
+console.log(isGithubActions)
+console.log(process.env && process.env.GITHUB_REPOSITORY)
+// console.log(process.env && process.env.GITHUB_REPOSITORY.replace(/.*?\//, ''))
 
-module.exports = {
+// module.exports = {
   // Other configurations...
-  output: 'export',
+//   output: 'export',
+//   basePath: '/givher',
   // Set basePath conditionally
-  basePath: isGithubActions ? `/${process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')}` : '',
+//   basePath: isGithubActions ? `/${process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')}` : '',
 
   // Set assetPrefix conditionally
-  assetPrefix: isGithubActions ? `/${process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')}` : '',
-};
+//   assetPrefix: isGithubActions ? `/${process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')}` : '',
+// };
+
+
+// const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = ''
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+module.exports = {
+  output: 'export',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+}
 

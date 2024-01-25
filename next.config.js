25911aa9d7587cec8e873827dacdafedd23fe3ec
Leigh Dahlin <leigh.dahlin@gmail.com>
2024-01-25 15:03:47 +0100

    try different basePath and assetPrefix to fix image urls

--- next.config.js
+++ next.config.js
@@ -1,21 +1,24 @@
 /** @type {import('next').NextConfig} */
 
-// const repo = 'givher'
-// const assetPrefix = `/${repo}/`
-// const basePath = `/${repo}`
-// const nextConfig = {
-//     assetPrefix: assetPrefix,
-//     basePath: basePath,
-// }
+const repo = 'givher'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+const nextConfig = {
+    output: 'export',
+    assetPrefix: assetPrefix,
+    basePath: basePath,
+}
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
+module.exports = nextConfig;
 
-module.exports = {
-  // Other configurations...
-  output: 'export',
-  // Set basePath conditionally
-  basePath: isGithubActions ? `/${process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')}` : '',
+// const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
-  // Set assetPrefix conditionally
-  assetPrefix: isGithubActions ? `/${process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')}/` : '',
-};
+// module.exports = {
+//   // Other configurations...
+//   output: 'export',
+//   // Set basePath conditionally
+//   basePath: isGithubActions ? `/${process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')}` : '',
+
+//   // Set assetPrefix conditionally
+//   assetPrefix: isGithubActions ? `/${process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')}/` : '',
+// };

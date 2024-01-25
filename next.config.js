01fb632aa41cd9004201bae8b48cba81dacd12eb
Leigh Dahlin <leigh.dahlin@gmail.com>
2024-01-25 11:17:55 +0100

    gh-pages config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+// const repo = 'givher'
+// const assetPrefix = `/${repo}/`
+// const basePath = `/${repo}`
+// const nextConfig = {
+//     assetPrefix: assetPrefix,
+//     basePath: basePath,
+// }
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+}

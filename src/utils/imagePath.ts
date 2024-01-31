b9c58ca018aa754949c695b1bef034e67787be62
Leigh Dahlin <leigh.dahlin@gmail.com>
2024-01-31 15:19:39 +0100

    code cleanup

--- src/utils/imagePath.ts
+++ src/utils/imagePath.ts
@@ -1,6 +1,5 @@
 export const getImagePath = (imageName:string) => {
     const isProduction = process.env.GITHUB_ACTIONS || false;
-    // return isProduction ?  `/givher/images/${imageName}`: `images/${imageName}` ;
     return `images/${imageName}`;
   };
   

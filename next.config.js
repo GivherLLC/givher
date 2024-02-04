33edad84252c48cd88891a23b22860775fed81ea
Leigh Dahlin <leigh.dahlin@gmail.com>
2024-02-04 13:29:25 +0100

    clean up next config file

--- next.config.js
+++ next.config.js
@@ -1,20 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
-console.log("----------------------------")
-console.log(`isGithubActions: ${isGithubActions}`)
-console.log("++++++++++++++++++++++++++++")
-console.log(`isGithubActionDeployPR: ${process.env.GITHUB_WORKFLOW || false}`)
-console.log("++++++++++++++++++++++++++++")
-console.log("process.env.GITHUB_ACTIONS")
-console.log(process.env.GITHUB_ACTIONS)
-console.log("++++++++++++++++++++++++++++")
-console.log("process.env")
-console.log(process.env)
-console.log("----------------------------")
 
-// let assetPrefix = ''
-// let basePath = ''
+let assetPrefix = ''
+let basePath = ''
 
 if(isGithubActions){
   const isDeployPR = process.env.GITHUB_WORKFLOW || false;

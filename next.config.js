00e6a1aa7e60e68f4cfd8cd7e32f03027e26b59b
Leigh Dahlin <leigh.dahlin@gmail.com>
2024-02-04 12:04:00 +0100

    add asset prefix for build PR workflow

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const isGithubActions = process.env.GITHUB_ACTIONS || false;
 console.log("----------------------------")
 console.log(`isGithubActions: ${isGithubActions}`)
 console.log("++++++++++++++++++++++++++++")
+console.log(`isGithubActionDeployPR: ${process.env.GITHUB_WORKFLOW || false}`)
+console.log("++++++++++++++++++++++++++++")
 console.log("process.env.GITHUB_ACTIONS")
 console.log(process.env.GITHUB_ACTIONS)
 console.log("++++++++++++++++++++++++++++")
@@ -13,7 +15,21 @@ console.log("++++++++++++++++++++++++++++")
 console.log(`isGithubActions: ${isGithubActions}`)
 console.log("----------------------------")
 
+let assetPrefix = ''
+let basePath = ''
+
+if(isGithubActions){
+  const isDeployPR = process.env.GITHUB_WORKFLOW || false;
+  if(isDeployPR){
+    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+    assetPrefix = `/${repo}`
+    basePath = `/${repo}`
+  }
+}
+
 module.exports = {
   output: 'export',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 }
 

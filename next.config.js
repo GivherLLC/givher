f2abacd7937f435d49f3e88d1120eccf22889ce2
Leigh Dahlin <leigh.dahlin@gmail.com>
2024-02-04 11:48:49 +0100

    remove custom url and add console logs to next.js config to debug

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+console.log("----------------------------")
+console.log(`isGithubActions: ${isGithubActions}`)
+console.log("++++++++++++++++++++++++++++")
+console.log("process.env.GITHUB_ACTIONS")
+console.log(process.env.GITHUB_ACTIONS)
+console.log("++++++++++++++++++++++++++++")
+console.log("process.env")
+console.log(process.env)
+console.log("++++++++++++++++++++++++++++")
+console.log(`isGithubActions: ${isGithubActions}`)
+console.log("----------------------------")
+
 module.exports = {
   output: 'export',
 }

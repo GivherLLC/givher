f721f785f0c01352106bde15bdf8ee0a29371521
Leigh Dahlin <leigh.dahlin@gmail.com>
2024-03-12 16:41:59 +0100

    fix link path util

--- src/utils/linkPath.ts
+++ src/utils/linkPath.ts
@@ -1,3 +1,3 @@
 export const getLinkPath = (linkPath: string) => {
-    return process.env.NEXT_PUBLIC_BASE_PATH ? `${process.env.NEXT_PUBLIC_BASE_PATH}/${linkPath}` : linkPath;
+    return process.env.NEXT_PUBLIC_BASE_PATH ? `${process.env.NEXT_PUBLIC_BASE_PATH}${linkPath}` : linkPath;
   };

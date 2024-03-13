08c0655db4c7b05a600b4ea121939c024dab0054
Leigh Dahlin <leigh.dahlin@gmail.com>
2024-03-13 10:29:36 +0100

    check if Link uses correct routing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect } from "react";
 import { getAssetPath } from "@/utils/assetPath";
-import { getLinkPath } from "@/utils/linkPath";
+// import { getLinkPath } from "@/utils/linkPath";
 import useIsMobile from "@/hooks/useIsMobile";
 import Link from "next/link";
 
@@ -57,7 +57,7 @@ export default function Navbar(){
         <header className="sticky top-0 z-40 shadow-lg bg-softOpal dark:bg-navySmoke flex justify-center">
             <div className="max-w-[85.75rem] w-full flex flex-row justify-between py-[1rem] mx-[0.625rem] lg:mx-[1.5625rem] relative">
                 <button onClick={()=>{scrollToTop()}}>
-                    <Link href={getLinkPath("/")}>
+                    <Link href="/">
                     <img
                         src={getAssetPath("/images/GIVHER_Primary_NavySmoke.png")}
                         alt="Givher Logo"
@@ -104,10 +104,10 @@ export default function Navbar(){
                 </div> */}
                 <nav className="flex flex-row gap-[1rem] font-medium items-center">
                     <ul className="hidden sm:flex justify-center items-center gap-[3rem]">
-                        <li className="w-[66px] flex justify-center"><a href={getLinkPath("/clients/")} className="text-navySmoke dark:text-electricYellow hover:font-bold transition-font duration-200 ease-in-out">Clients</a></li>
-                        <li className="w-[66px] flex justify-center"><a href={getLinkPath("/events/")} className="text-navySmoke dark:text-electricYellow hover:font-bold transition-font duration-200 ease-in-out">Events</a></li>
-                        <li className="w-[66px] flex justify-center"><a href={getLinkPath("/team/")} className="text-navySmoke dark:text-electricYellow hover:font-bold transition-font duration-200 ease-in-out">Team</a></li>
-                        <li className="w-[66px] flex justify-center"><a href={getLinkPath("/contact/")} className="text-navySmoke dark:text-electricYellow hover:font-bold transition-font duration-200 ease-in-out">Contact</a></li>
+                        <li className="w-[66px] flex justify-center"><Link href="/clients/" className="text-navySmoke dark:text-electricYellow hover:font-bold transition-font duration-200 ease-in-out">Clients</Link></li>
+                        <li className="w-[66px] flex justify-center"><Link href="/events/" className="text-navySmoke dark:text-electricYellow hover:font-bold transition-font duration-200 ease-in-out">Events</Link></li>
+                        <li className="w-[66px] flex justify-center"><Link href="/team/" className="text-navySmoke dark:text-electricYellow hover:font-bold transition-font duration-200 ease-in-out">Team</Link></li>
+                        <li className="w-[66px] flex justify-center"><Link href="/contact/" className="text-navySmoke dark:text-electricYellow hover:font-bold transition-font duration-200 ease-in-out">Contact</Link></li>
                     </ul>
                     <button type="button" onClick={()=>{setNavOpen(!navOpen)}} className="flex sm:hidden relative flex-col items-center justify-between h-[50px] w-[50px] p-0 ml-auto" aria-label="Open/Close Menu">
                         <div className={`h-[2px] w-full bg-navySmoke dark:bg-electricYellow absolute right-[50%] max-w-[20px] top-[50%] transition-all duration-250 ease-in mt-[-5px] ${navOpen ? "transform -translate-x-1/2 -translate-y-1/2 rotate-[-45deg] mt-0":""}`}/>
@@ -119,10 +119,10 @@ export default function Navbar(){
             {navOpen && (
                     <div className="fixed top-[58px] h-screen w-screen bg-softOpal dark:bg-navySmoke">
                         <nav className="flex flex-col items-center font-visbyBold gap-10 p-5">
-                            <a href={getLinkPath("/clients/")} onClick={()=>{setNavOpen(!navOpen)}} className="text-navySmoke dark:text-electricYellow hover:text-navySmoke">Clients</a>
-                            <a href={getLinkPath("/events/")} onClick={()=>{setNavOpen(!navOpen)}} className="text-navySmoke dark:text-electricYellow hover:text-navySmoke">Events</a>
-                            <a href={getLinkPath("/team/")} onClick={()=>{setNavOpen(!navOpen)}} className="text-navySmoke dark:text-electricYellow hover:text-navySmoke">Team</a>
-                            <a href={getLinkPath("/contact/")} onClick={()=>{setNavOpen(!navOpen)}} className="text-navySmoke dark:text-electricYellow hover:text-navySmoke">Contact</a>
+                            <Link href="/clients/" onClick={()=>{setNavOpen(!navOpen)}} className="text-navySmoke dark:text-electricYellow hover:text-navySmoke">Clients</Link>
+                            <Link href="/events/" onClick={()=>{setNavOpen(!navOpen)}} className="text-navySmoke dark:text-electricYellow hover:text-navySmoke">Events</Link>
+                            <Link href="/team/" onClick={()=>{setNavOpen(!navOpen)}} className="text-navySmoke dark:text-electricYellow hover:text-navySmoke">Team</Link>
+                            <Link href="/contact/" onClick={()=>{setNavOpen(!navOpen)}} className="text-navySmoke dark:text-electricYellow hover:text-navySmoke">Contact</Link>
                         </nav>
                     </div>
                 )}

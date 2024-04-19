179a4428094e0c6964319be060b2f550fbf730d4
Leigh Dahlin <leigh.dahlin@gmail.com>
2024-04-19 12:11:30 +0200

    remove next.js image component and config, unavailable with github page

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 
 module.exports = {
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
 }
--- src/components/home/AboutUs.tsx
+++ src/components/home/AboutUs.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { getAssetPath } from "@/utils/assetPath";
-import Image from "next/image";
 
 type AboutUsProps = {
     aboutTitle: string;
@@ -29,8 +28,8 @@ export default function AboutUs({aboutInfo}:{aboutInfo:AboutUsProps}){
                                     <p className="h-full flex items-center text-black dark:text-softOpal">{s.sectionText}</p>
                                 </div>
                                 <div className={`w-full flex justify-center items-center ${even ? "lg:justify-end":"lg:justify-start"}`}>
-                                    <Image src={getAssetPath(s.sectionImageSrc)} alt={s.sectionImageAlt} height={237.5} width={500} className="sm:max-w-[300px] lg:max-w-[500px] xl:max-w-[600px] max-h-[285px] dark:hidden"/>
-                                    <Image src={getAssetPath(s.sectionImageSrcDarkMode)} alt={s.sectionImageAlt} height={237.5} width={500} className="max-w-[300px] lg:max-w-[500px] xl:max-w-[600px] max-h-[285px] hidden dark:block"/>
+                                    <img src={getAssetPath(s.sectionImageSrc)} alt={s.sectionImageAlt} height={237.5} width={500} className="sm:max-w-[300px] lg:max-w-[500px] xl:max-w-[600px] max-h-[285px] dark:hidden"/>
+                                    <img src={getAssetPath(s.sectionImageSrcDarkMode)} alt={s.sectionImageAlt} height={237.5} width={500} className="max-w-[300px] lg:max-w-[500px] xl:max-w-[600px] max-h-[285px] hidden dark:block"/>
                                 </div>
                             </div>
                         )

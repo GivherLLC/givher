f20d5b663e626cf090ed5c72b8313d38c72e9a5d
Leigh Dahlin <leigh.dahlin@gmail.com>
2024-09-24 17:32:32 +0200

    add additional security to links leading away from the website

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -17,7 +17,7 @@ export default function Footer(){
                     <div className="w-full md:w-1/3 flex flex-col-reverse md:flex-col items-center justify-between gap-[2rem] mb-[1rem] md:mb-[0rem]">
                         <div data-id="footer-icons" className="flex gap-[2rem]">
                             {footerData.iconLinks.map((i)=>(
-                                <Link key={i.iconImageAlt} href={i.iconLink} target="_blank">
+                                <Link key={i.iconImageAlt} href={i.iconLink} target="_blank" rel="noopener noreferrer">
                                     <img src={getAssetPath(i.iconImageSrc)} loading="lazy" alt={i.iconImageAlt} height={30} width={30} className="dark:hidden hover:scale-[1.1] transition-transform ease-in-out"/>
                                     <img src={getAssetPath(i.darkmodeSrc)} loading="lazy" alt={i.iconImageAlt} height={30} width={30} className="hidden dark:block hover:scale-[1.1] transition-transform ease-in-out"/>
                                 </Link>
@@ -32,18 +32,18 @@ export default function Footer(){
                         <ul className="w-full flex flex-col items-center md:items-end h-full">
                             {footerData.pageLinks.map((l)=>(
                                 <li key={l.linkText} className="text-navySmoke dark:text-softOpal font-medium hover:font-semibold transition ease-in-out mb-[1rem] w-[110px] text-center md:text-left">
-                                    <Link href={l.link} {...(l.external ? { target: "_blank" } : {})}>{l.linkText}</Link>
+                                    <Link href={l.link} {...(l.external ? { target: "_blank", rel: "noopener noreferrer" } : {})}>{l.linkText}</Link>
                                 </li>
                             ))}
                         </ul>
                         <div className="text-xs text-right hidden md:flex md:justify-end text-navySmoke dark:text-softOpal whitespace-nowrap gap-[5px]">
-                            Made with <span className="dark:hidden">&#128420;</span><span className=" hidden dark:block">&#x1F90D;</span> by <Link href="https://leighdahlin.com/" className="underline" target="_blank">Leigh Dahlin</Link>
+                            Made with <span className="dark:hidden">&#128420;</span><span className=" hidden dark:block">&#x1F90D;</span> by <Link href="https://leighdahlin.com/" className="underline" target="_blank" rel="noopener noreferrer">Leigh Dahlin</Link>
                         </div>
                     </div>
                 </div>
             </div>
             <div className="text-xs mb-[1rem] text-right flex md:hidden md:justify-end text-navySmoke dark:text-softOpal whitespace-nowrap gap-[5px]">
-                Made with <span className="dark:hidden">&#128420;</span><span className=" hidden dark:block">&#x1F90D;</span> by <Link href="https://leighdahlin.com/" className="underline" target="_blank">Leigh Dahlin</Link>
+                Made with <span className="dark:hidden">&#128420;</span><span className=" hidden dark:block">&#x1F90D;</span> by <Link href="https://leighdahlin.com/" className="underline" target="_blank" rel="noopener noreferrer">Leigh Dahlin</Link>
             </div>        
     </footer>
     )

f0c1e20a539c710bc3243ee8c8b0be04cb7f6000
Leigh Dahlin <leigh.dahlin@gmail.com>
2024-09-27 11:25:04 +0200

    update types for field updates in cms

--- src/types/types.ts
+++ src/types/types.ts
@@ -8,15 +8,19 @@ export type EventType = {
     eventCity:string;
     eventLocation: string | null;
     clientName:string;
-    eventButtonText:string;
-    eventButtonLink:string;
+    eventButtonTextOne: string;
+    eventButtonLinkOne: string;
+    eventButtonTextTwo: string | null;
+    eventButtonLinkTwo: string | null;
     eventDescription:EventDescriptionItem[] | null;
     boldedEventInformation:BoldedEventInformationItem[] | null;
-    eventLink:string;
-    eventLinkText:string;
     detailImage:string;
     postponed: boolean;
 }
+
+export type UpcomingClientEventType = (EventType | ComingSoonEventType) & {
+    comingSoon: boolean;
+  };
 
 type EventDescriptionItem = {
     paragraph: string;
@@ -28,21 +32,28 @@ type BoldedEventInformationItem = {
 
 export type FeaturedEventType = {
     eventName: string;
-    eventCity: string;
-    firstDayOfEvent: string;
-    lastDayOfEvent:string | null;
-    eventButtonText: string;
-    eventButtonLink: string;
+    eventCity: string | null;
+    eventButtonTextOne: string | null;
+    eventButtonLinkOne: string | null;
     clientImage: string;
+    firstDayOfEvent: string | null;
+    timeOfYear: string | null;
+    lastDayOfEvent:string | null;
+    comingSoon: boolean;
 }
 
 export type ComingSoonEventType = {
     clientName:string;
     eventName:string;
-    eventButtonText:string;
-    eventButtonLink:string;
-    eventDateString:string;
-    eventCity:string;
+    firstDayOfEvent:string | null;
+    lastDayOfEvent:string | null;
+    timeZone: string | null;
+    timeOfYear: string | null;
+    eventButtonTextOne: string | null;
+    eventButtonLinkOne: string | null;
+    eventButtonTextTwo: string | null;
+    eventButtonLinkTwo: string | null;
+    eventCity:string | null;
     postponed:boolean;
 }
 
@@ -67,7 +78,7 @@ export type TeamMember = {
 
 export type Client = {
     clientName: string;
-    clientImageSrc: string;
+    clientLogo: string;
     clientWebsite: string;
     clientW9Src: string;
 }
@@ -77,6 +88,6 @@ export interface ClientImage {
 }
 
 export interface ClientLogo {
-    logoSrc: string;
+    clientLogo: string;
     logoAlt: string;
 }

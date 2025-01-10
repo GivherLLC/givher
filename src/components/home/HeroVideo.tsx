7159ac2fb2db296d00481915026dd7e8b20ac335
Leigh Dahlin <leigh.dahlin@gmail.com>
2025-01-10 19:45:33 +0100

    update homepage video and layout

--- src/components/home/HeroVideo.tsx
+++ src/components/home/HeroVideo.tsx
@@ -15,29 +15,43 @@ export default function HeroVideo({ data }: HeroVideoProps) {
     data;
 
   return (
-    <div className="flex flex-col justify-start overflow-hidden">
-      <div className="bg-black h-[calc(100vh-65px)] relative">
-        <div className="relative flex flex-col items-center justify-center w-full h-full left-0 mx-auto p-[1.25rem] text-center top-0 z-10">
-          <div className="h-full flex flex-col items-center justify-end pb-[50px] gap-[2.5rem]">
-            <h1 className="font-visbyBold text-softOpal">{videoTitle}</h1>
-            <p className="text-softOpal max-w-[350px]">{videoDescription}</p>
-            <ButtonLink
-              bg="softOpal"
-              darkModeBg="transparent"
-              text={videoButtonText}
-              link={videoButtonLink}
-            />
-          </div>
+    <div className="bg-softOpal dark:bg-navySmoke flex flex-col justify-start overflow-hidden lg:min-h-[600px] px-[0.625rem] md:px-[1.5625rem] lg:px-0">
+      <div className="lg:hidden flex flex-col justify-center items-center py-[1.5rem] px-[0.625rem] md:px-[1.5625rem]">
+        <h1 className="font-visbyBold text-navySmoke dark:text-softOpal text-center max-w-[450px]">
+          {videoTitle}
+        </h1>
+      </div>
+      <div className="bg-black lg:h-[calc(100vh-65px)] w-full relative z-10 flex justify-center">
+        <div className="hidden absolute lg:flex w-full max-w-[85.75rem] px-[0.625rem] md:px-[1.5625rem] flex-col items-center lg:items-start text-center lg:text-left justify-end gap-[1rem] bottom-[100px] z-10">
+          <h1 className="font-visbyBold text-softOpal max-w-[450px]">
+            {videoTitle}
+          </h1>
+          <p className="text-softOpal max-w-[350px]">{videoDescription}</p>
+          <ButtonLink
+            bg="softOpal"
+            darkModeBg="transparent"
+            text={videoButtonText}
+            link={videoButtonLink}
+          />
         </div>
         <video
           id="home-video"
           autoPlay
           loop
           muted
+          controls
           playsInline
           aria-hidden={true}
-          src={'/videos/givher-video.hevc.mp4'}
-          className="w-full h-full bg-black object-cover absolute overflow-clip top-0 opacity-75"
+          src={'/videos/final-video.hevc.mp4'}
+          className="lg:w-full lg:h-full lg:object-cover lg:opacity-75 aspect-video"
+        />
+      </div>
+      <div className="lg:hidden flex flex-col justify-center items-center py-[1rem] px-[0.625rem] md:px-[1.5625rem]">
+        <ButtonLink
+          bg="mauvelous"
+          darkModeBg="transparent"
+          text={videoButtonText}
+          link={videoButtonLink}
         />
       </div>
     </div>

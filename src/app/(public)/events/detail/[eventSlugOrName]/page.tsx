0288eb5afdaad9f18d3af789152de2f22ea6f284
Leigh Dahlin <leigh.dahlin@gmail.com>
2025-05-10 08:13:13 +0200

    downgrade next and react to last major version and revent event detail page generation file to what it was before the upgrade

--- src/app/(public)/events/detail/[eventSlugOrName]/page.tsx
+++ src/app/(public)/events/detail/[eventSlugOrName]/page.tsx
@@ -1,5 +1,4 @@
 export const revalidate = 43200;
-export const dynamic = 'force-dynamic';
 
 import React from 'react';
 import EventDetailPage from '@/components/event-detail/EventDetailPage';
@@ -12,49 +11,49 @@ import {
   getClientEvents,
 } from '../../../../../../lib/getAllEvents';
 import getAllClientImages from '../../../../../../lib/getAllClientImages';
+import { EventDetailPageProps } from '@/types/types';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export async function generateMetadata(props: any): Promise<Metadata> {
-  const params = await props.params;
-  const decodedParam = decodeURIComponent(params.eventSlugOrName);
+export async function generateMetadata({
+  params: { eventSlugOrName },
+}: EventDetailPageProps): Promise<Metadata> {
+  const decodedParam = decodeURIComponent(eventSlugOrName);
 
   const event = await getEventBySlugOrName(decodedParam);
 
-  if (!event) {
-    return { title: 'Event Not Found' };
-  }
-
-  const title = `${event.eventName} | Givher Event`;
-  const description = `Event details for ${event.clientName}'s event ${event.eventName}`;
-  const url = `/events/detail/${event.slug}`;
-
-  return {
-    title,
-    description,
-    openGraph: {
+  if (event) {
+    const title = `${event.eventName} | Givher Event`;
+    const description = `Event details for ${event.clientName}'s event ${event.eventName}`;
+    const url = `/events/detail/${event.slug}`; // Ensure slug is used in the URL
+    return {
       title,
       description,
-      url,
-      siteName: 'Givher',
-      type: 'website',
-      images: [
-        {
-          url: 'https://www.givher.com/opengraph-image.png',
-          width: 1200,
-          height: 630,
-          alt: 'Givher Political Hospitality',
-        },
-      ],
-    },
-  };
+      openGraph: {
+        title,
+        description,
+        url,
+        siteName: 'Givher',
+        type: 'website',
+        images: [
+          {
+            url: 'https://www.givher.com/opengraph-image.png',
+            width: 1200,
+            height: 630,
+            alt: 'Givher Political Hospitality',
+          },
+        ],
+      },
+    };
+  } else {
+    return {
+      title: 'Event Not Found',
+    };
+  }
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default async function EventsDetailPage(props: any) {
-  const params = await props.params;
-  const decodedParam = decodeURIComponent(params.eventSlugOrName);
-
+export default async function EventsDetailPage({
+  params: { eventSlugOrName },
+}: EventDetailPageProps) {
+  const decodedParam = decodeURIComponent(eventSlugOrName);
   const eventsPageData = getEventsPageData();
   const [event, clientImages, clientEvents] = await Promise.all([
     getEventBySlugOrName(decodedParam),
@@ -62,31 +61,33 @@ export default async function EventsDetailPage(props: any) {
     getClientEvents(decodedParam),
   ]);
 
-  if (!event) {
+  if (event) {
+    const clientImage = clientImages[event.clientName];
+
     return (
-      <div className="bg-white dark:bg-navySmoke w-full min-h-[calc(100vh-408px)] flex items-center justify-center">
-        <h1 className="font-visbyBold text-mauvelous dark:text-softOpal">
-          Oops! Event not found.
-        </h1>
-      </div>
+      <EventDetailPage
+        event={event}
+        clientEvents={clientEvents}
+        postponedEventText={eventsPageData.postponedEventText}
+        clientImage={clientImage}
+      />
     );
   }
 
-  const clientImage = clientImages[event.clientName];
-
   return (
-    <EventDetailPage
-      event={event}
-      clientEvents={clientEvents}
-      postponedEventText={eventsPageData.postponedEventText}
-      clientImage={clientImage}
-    />
+    <div className="bg-white dark:bg-navySmoke w-full min-h-[calc(100vh-408px)] flex items-center justify-center">
+      <h1 className="font-visbyBold text-mauvelous dark:text-softOpal">
+        Oops! Event not found.
+      </h1>
+    </div>
   );
 }
 
 export async function generateStaticParams() {
-  const events = await getReadyEvents();
-  return events.map((event) => ({
-    eventSlugOrName: event.slug || getEventNameParam(event.eventName),
+  const events = await getReadyEvents(); // Only includes fully ready events
+  const params = events.map((event) => ({
+    eventSlugOrName: event.slug || getEventNameParam(event.eventName), // Prioritize slug for static params
   }));
+
+  return params;
 }
